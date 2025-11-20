0636e96b138543b192cbf5081ebf65d9921109dd
David2008-coder <obasukedavid47@gmail.com>
2025-11-19 16:52:02 -0800

    Fix component imports for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@
 import React, { useState } from 'react';
 
 // IMPORT COMPONENTS
-import  Navbar  from '/Components/Navbar.jsx';
+import  Navbar  from './Components/Navbar.jsx';
 import  Hero  from './Components/Hero.jsx';
 import  Products  from './Components/Products.jsx';
 import  Footer  from './Components/Footer.jsx';
